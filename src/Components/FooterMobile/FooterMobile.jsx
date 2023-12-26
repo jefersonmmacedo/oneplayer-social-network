041fd50c369712b090881b8a44a006e283023969
jefersonmmacedo <jefersonmacedowgf@gmail.com>
2023-12-26 17:13:10 -0300

    Adapta~]ao mobile

--- src/Components/FooterMobile/FooterMobile.jsx
+++ src/Components/FooterMobile/FooterMobile.jsx
@@ -2,6 +2,7 @@ import "./footerMobile.css"
 import { GoPlusCircle } from "react-icons/go";
 import { HiOutlineVideoCamera } from "react-icons/hi";
 import { IoCompass, IoCompassOutline, IoHome, IoHomeOutline } from "react-icons/io5";
+import { GoHome } from "react-icons/go";
 import { GoVideo } from "react-icons/go";
 import { TfiVideoClapper } from "react-icons/tfi";
 import { BiMoviePlay } from "react-icons/bi";
@@ -11,7 +12,7 @@ import photo from "../../assets/images/default.png";
 export function FooterMobile() {
     return (
         <div className="FooterMobile">
-                <a href="/" ><IoHomeOutline /></a>
+                <a href="/" ><GoHome /></a>
                 <a href="/explorar" ><IoCompassOutline /> </a>
              <button ><GoPlusCircle  /></button>
              <a href="/reels" ><BsPlayBtn /> </a>
