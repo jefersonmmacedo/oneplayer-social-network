6deb2d3efecec6c2c4befe9114bd100c17d3e211
jefersonmmacedo <jefersonmacedowgf@gmail.com>
2023-12-27 22:11:19 -0300

    Trending e Seguidores

--- src/Components/SearchTrending/SearchTrending.jsx
+++ src/Components/SearchTrending/SearchTrending.jsx
@@ -1,6 +1,14 @@
 import { CiSaveDown2 } from "react-icons/ci";
 import "./searchTrending.css";
 import { IoCalendarOutline, IoChatbubbleEllipsesOutline, IoHeart, IoHeartOutline, IoSearchSharp } from "react-icons/io5";
+import { HiCheckBadge } from "react-icons/hi2";
+import { SlOptionsVertical } from "react-icons/sl";
+import photo from "../../assets/images/default.png";
+import photo2 from "../../assets/images/default2.png";
+import photo3 from "../../assets/images/default3.png";
+import photo4 from "../../assets/images/default4.png";
+
+
 
 export function SearchTrending() {
     return (
@@ -10,7 +18,6 @@ export function SearchTrending() {
                 <button ><CiSaveDown2 /></button> 
                 <button ><IoHeartOutline /></button>
                 <button ><IoChatbubbleEllipsesOutline /></button>
-                
             </div>
             <div className="search">
                 <IoSearchSharp />
@@ -18,7 +25,114 @@ export function SearchTrending() {
             </div>
 
             <div className="asideTrending">
+                <h5>Próximos eventos</h5>
+            </div>
+            <div className="asideTrending">
                 <h5>O que está acontecendo</h5>
+                <div className="topics">
+                    <div className="topicUnic">
+                        <div className="textsTopics">
+                            <h6>Olimpiadas - Assuntos do momento</h6>
+                            <h5>#voleifeminino</h5>
+                            <h6>2.487 posts</h6>
+                        </div>
+
+                       <SlOptionsVertical />
+                    </div>
+                    <div className="topicUnic">
+                        <div className="textsTopics">
+                            <h6>Neymar - Assuntos do momento</h6>
+                            <h5>#cruzeirodoneymar</h5>
+                            <h6>5.487 posts</h6>
+                        </div>
+
+                       <SlOptionsVertical />
+                    </div>
+                    <div className="topicUnic">
+                        <div className="textsTopics">
+                            <h6>Libertadores - Assuntos do momento</h6>
+                            <h5>#fluminense</h5>
+                            <h6>5.487 posts</h6>
+                        </div>
+
+                       <SlOptionsVertical />
+                    </div>
+                    <div className="topicUnic">
+                        <div className="textsTopics">
+                            <h6>Mundial de clubes - Assuntos do momento</h6>
+                            <h5>#cityxflu</h5>
+                            <h6>5.487 posts</h6>
+                        </div>
+
+                       <SlOptionsVertical />
+                    </div>
+                </div>
+            </div>
+            <div className="asideTrending">
+                <h5>Sugestão para seguir</h5>
+
+                <div className="usersList">
+
+                    <div className="userUnic">
+                        <div className="blockUser">
+
+                        <img src={photo} alt="" />
+                            <div className="username">
+                        <h6>Jeferson Macedo</h6>
+                        <span>jefersonmmacedo</span>
+
+                            </div>
+
+                        <HiCheckBadge />
+                        </div>
+                        <button>+ Seguir</button>
+                    </div>
+                    
+
+                    <div className="userUnic">
+                    <div className="blockUser">
+
+                        <img src={photo2} alt="" />
+                            <div className="username">
+                        <h6>Bruno Motta</h6>
+                        <span>bmottaefg</span>
+
+                            </div>
+
+                        <HiCheckBadge />
+                    </div>
+                        <button>+ Seguir</button>
+                    </div>
+
+                    <div className="userUnic">
+                    <div className="blockUser">
+
+                        <img src={photo3} alt="" />
+                            <div className="username">
+                        <h6>Ana Souza</h6>
+                        <span>souzaaninha</span>
+
+                            </div>
+
+                        <HiCheckBadge />
+                    </div>
+                        <button>+ Seguir</button>
+                    </div>
+
+                    <div className="userUnic">
+                        <div className="blockUser">
+                        <img src={photo4} alt="" />
+                            <div className="username">
+                        <h6>Marcos Baraga</h6>
+                        <span>bragamarcosmkt</span>
+                            </div>
+
+                        <HiCheckBadge />
+                        </div>
+                        <button>+ Seguir</button>
+                    </div>
+
+                </div>
             </div>
         </div>
     )
